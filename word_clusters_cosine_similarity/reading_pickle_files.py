c54f7fc4d57cf9eacdcf1036e51401d236c99bd0
mjb-0001 <56898558+mjb-0001@users.noreply.github.com>
2021-05-20 19:01:37 -0500

    Add files via upload

---
+++ word_clusters_cosine_similarity/reading_pickle_files.py
@@ -0,0 +1,36 @@
+
+------------------------------------------------------------
+#DICT_COVID_CLUSTER_TO_PULMONARY_20_CLUSTERS.pkl
+
+import pickle as pkl
+
+with open('DICT_COVID_CLUSTER_TO_PULMONARY_20_CLUSTERS.pkl', 'rb') as f:
+    pickle_file_covid = pkl.load(f)
+print(len(pickle_file_covid))
+
+
+##This results in length 47838
+
+------------------------------------------------------------
+
+
+
+
+
+
+------------------------------------------------------------
+
+#DICT_PULMONARY_CLUSTER_TO_COVID_20_CLUSTERS.pkl
+
+import pickle as pkl
+
+with open('DICT_PULMONARY_CLUSTER_TO_COVID_20_CLUSTERS.pkl', 'rb') as f:
+    pickle_file_pulm = pkl.load(f)
+print(len(pickle_file_pulm))
+
+
+##This results in length 57889
+
+
+
+------------------------------------------------------------
